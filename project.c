254cfce1c6d61a9e629bd4ce7871366369194e34
cortes28 <54484088+cortes28@users.noreply.github.com>
2021-03-02 17:44:26 -0800

    Update project.c

--- project.c
+++ project.c
@@ -5,22 +5,24 @@
 //our table for tic-tac-toe
 int playerMoves(char player, int status, int count);
 
+//display tic-tac-toe table
 void display();
 
+//our 2D array table for our game
 char table[ROW][COLUMN];
 
-//this status variable will be in use to see if tic tac toe game has been completed or not
-//int myRow, myColumn;
-int count;
 //a counter to see if the table is maxed and no winner->Cat's game
+int count;
 
 int main()
 {
+  //status set to 1 and count to 0 as the game will start
   char player;
   int status = 1;
   count = 0;
   player = 'X';
 
+  //making the empty table
   for(int i = 0; i < 3; i++)
   {
     for(int j = 0; j< 3; j++)
@@ -28,23 +30,19 @@ int main()
       table[i][j] = ' ';
     }
   }
-  //making the empty table
-
-
-	//we are going to print the first move for player 1 here and get his move
 
 	printf("Welcome to Tic-Tac-Toe\n");
 	printf("======================\n");
 
-
-  //while the game has not fully ended, we
+  //while the game has not ended (game ends if status equals 0)
   do
   {
-
+    //print the player who is next
     if(player == 'X')  { printf("Player 1 turn '%c'\n", player); }
 
     else { printf("Player 2 turn '%c'\n", player); }
-
+    //display and count how many moves have been done and go into playerMoves
+    //function
     display();
     ++count;
 	  status = playerMoves(player, status, count);
@@ -59,16 +57,20 @@ int main()
 
 int playerMoves(char player, int status, int count)
 {
-int myRow, myColumn;
-myRow = myColumn = 0;
+  //our variables for getting the values of where the player wants to
+  //draw an 'X' or 'O'
+  int myRow, myColumn;
+  myRow = myColumn = 0;
 
+  //get values while the cell is available and the number is not out of bounds
   printf("Which Row would you like?\n");
   scanf("%d", &myRow);
 
   printf("Which Column would you like?\n");
   scanf("%d", &myColumn);
 
-  while (table[myRow][myColumn] == 'X' || table[myRow][myColumn] == 'O' || myRow > 2 || myColumn > 2)
+  while (table[myRow][myColumn] == 'X' || table[myRow][myColumn] == 'O' ||
+         myRow > 2 || myRow < 0 || myColumn > 2 || myColumn < 0)
 	{
 		printf("Illegal Move, enter a valid move\n");
 
@@ -78,44 +80,42 @@ myRow = myColumn = 0;
 		printf("Which Column would you like?\n");
 		scanf("%d", &myColumn);
 	}
-  // display();
-  //
-  // printf("\nBefore insertion\n");
-  //
-  // // int row = myRow;
-  // // int col = myColumn;
-  //
-	  table[myRow][myColumn] = player;
+
+  //plug into the table once the value is valid
+  table[myRow][myColumn] = player;
 
   //Check if game should be ended if player won
+  //checks if player won across a column or row
 	for (int i = 0; i < 2 && count > 2; i++)
 	{
-
-      if(player == table[i][0] && player == table[i][1] && player == table[i][2] || player == table[0][i] && player == table[1][i] && player == table[2][i])
-      {
-      printf("%c Player Wins!", player);
-      display();
-      return 0;
-      }
-
-      if(player == table[0][0] && player == table[1][1] && player == table[2][2] || player == table[0][2] && player == table[1][1] && player == table[2][0] )
-      {
-      printf("%c Player Wins!", player);
-      display();
-      return 0;
-      }
-	}
-
-  if(count == 8)
+    if(player == table[i][0] && player == table[i][1] && player == table[i][2] ||
+       player == table[0][i] && player == table[1][i] && player == table[2][i])
+    {
+    printf("%c Player Wins!", player);
+    display();
+    return 0;
+    }
+  }
+  //checks if player won diagonally
+  if(player == table[0][0] && player == table[1][1] && player == table[2][2] ||
+     player == table[0][2] && player == table[1][1] && player == table[2][0] )
+  {
+  printf("%c Player Wins!", player);
+  display();
+  return 0;
+  }
+  //if the counter has reached 8 meaning all moves have been done
+  //we end the game.
+  if(count == 9)
   {
     printf("\nCat's Game!\n");
     display();
     return 0;
   }
-
+  //returns status while the game continues
 	return status;
 }
-
+//our function to display our table of tic-tac-toe
 void display()
 {
   printf("\n");
@@ -126,4 +126,5 @@ void display()
   printf("-----\n");
 
   printf("%c|%c|%c\n", table[2][0], table[2][1], table[2][2]);
+  printf("\n");
 }
